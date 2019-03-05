0e89543da6f4a9bb18c15bd59a3239cdc2add8e2
hybridbad <hybridbad90@gmail.com>
2019-03-05 20:43:27 +0000

    Added predicate test for working, release_bike method creates a new object with an instance of Bike class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,4 @@
-require './lib/docking_station'
+require './lib/docking_station.rb'
 
 describe DockingStation do
   it 'repsonds to the release_bike method' do
@@ -9,4 +9,19 @@ end
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+end
+
+describe DockingStation do
+  it 'Gets a bike, and expects it to be working' do
+    station = DockingStation.new
+    bike = station.release_bike
+    expect(bike).to respond_to(:working?)
+  end
+end
+describe DockingStation do
+  it 'Gets a bike, and expects it to be working' do
+    station = DockingStation.new
+    bike = station.release_bike
+    expect(bike).to be_working
+  end
 end
