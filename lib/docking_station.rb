866833400a11809aa11c99ef3180bd0f4b59ab09
hybridbad <hybridbad90@gmail.com>
2019-03-05 18:29:10 +0000

    added release_bike method and test instance object responds to method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,2 +1,6 @@
-def DockingStation
+class DockingStation
+
+  def release_bike
+  end
+  
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,12 @@
 require './lib/docking_station'
 
 describe DockingStation do
+  it 'repsonds to the release_bike method' do
+    docking_station = DockingStation.new
+    expect(docking_station).to respond_to(:release_bike)
+  end
+end
+
+describe DockingStation do
+  it { is_expected.to respond_to :release_bike }
 end
